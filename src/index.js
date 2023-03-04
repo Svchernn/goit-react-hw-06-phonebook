b4d843364e7dd38f264ee52e82a719e3b5b16f01
Svitlana Cherniakova <107955029+Svchernn@users.noreply.github.com>
2023-03-04 09:59:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,10 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter
-          basename="/goit-react-hw-06-phonebook
-"
-        >
+        <BrowserRouter basename="/goit-react-hw-06-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
